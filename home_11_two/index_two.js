de8caa7d41074f0bfd3abb71632f248bf07aada7
KotAliaksandr <79219310+KotAliaksandr@users.noreply.github.com>
2021-03-24 17:26:09 +0300

    Add files via upload

---
+++ home_11_two/index_two.js
@@ -0,0 +1,16 @@
+let num = prompt('Введите целое значение от 1 до 4');
+
+switch (num) {
+    case '1':
+       console.log('Зима');
+    break;
+    case '2':
+        console.log('Весна');
+        break;
+    case '3':
+        console.log('Лето');
+        break;
+    case '4':
+        console.log('Осень');
+        break;    
+}
