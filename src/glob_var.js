d3fa5c5884766123597979520614707fca76621f
KotAliaksandr <79219310+KotAliaksandr@users.noreply.github.com>
2021-05-14 16:22:20 +0300

    Add files via upload

---
+++ src/glob_var.js
@@ -0,0 +1,4 @@
+export const toDoArr = [];
+export const inp = document.createElement('input');
+export const butt = document.createElement('button');
+export const ul = document.createElement('ul');
